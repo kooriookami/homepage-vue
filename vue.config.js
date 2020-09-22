b8a1d2bd1bf53b52e8a55da8bb8ee20a1569098e
kooriookami <bingshuanglingluo@163.com>
2020-09-22 11:24:34 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+    publicPath: './',
+    productionSourceMap: false, //打包不生成map文件
+    devServer: {
+        port: 8000
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `@import './src/style/variable.scss';`
+            }
+        }
+    },
+    configureWebpack: {
+        plugins: [
+            new CompressionPlugin({
+                test: /\.(js|css)$/,
+                threshold: 10240,   // 超过10k压缩
+                deleteOriginalAssets: false // 删除源文件
+            })
+        ]
+    }
+};
